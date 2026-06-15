2562b9525294d82bc9f9700c0fc914c92fde90f4
Sahil Adit <159920447+sahiladit@users.noreply.github.com>
2026-06-15 19:04:11 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyBm9ll4XIqL5oW2-xq49PmkDUbxERTJvf8",
-    authDomain: "rivendell-d6e99.firebaseapp.com",
-    projectId: "rivendell-d6e99",
-    storageBucket: "rivendell-d6e99.firebasestorage.app",
-    messagingSenderId: "155957508251",
-    appId: "1:155957508251:web:5ad5e9e7dd0c44fea5ee3e",
-    measurementId: "G-P22BWX7YSC"
+   apiKey: "key",
+    authDomain: "domain",
+    projectId: "id",
+    storageBucket: "bucketID",
+    messagingSenderId: "SenderID",
+    appId: "ID",
+    measurementId: "ID"
 };
 
 const app = initializeApp(firebaseConfig);
